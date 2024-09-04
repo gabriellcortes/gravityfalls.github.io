d2d905ce32ca07efdc057efc457f7256b050300d
Gabriel Lopes <55008126+gabriellcortes@users.noreply.github.com>
2024-09-04 19:20:07 -0300

    Update script_audio.js

--- script_audio.js
+++ script_audio.js
@@ -5,7 +5,7 @@ const audioPlayer = document.getElementById('audio-player');
 const audioSlider = document.getElementById('audio-slider');
 const draggableImage = document.getElementById('draggable-image');
 
-let startY = 0;
+let startX = 0;
 let isDragging = false;
 let audioChanged = false;
 
@@ -35,15 +35,16 @@ audioSlider.addEventListener('input', () => {
 });
 
 draggableImage.addEventListener('touchstart', (e) => {
-  startY = e.touches[0].clientY;
+  startX = e.touches[0].clientX;
   isDragging = true;
   draggableImage.style.cursor = 'grabbing';
 });
 
 draggableImage.addEventListener('touchmove', (e) => {
   if (isDragging) {
-    const deltaY = startY - e.touches[0].clientY;
-    if (deltaY > 50) { // Ajuste o valor conforme necessário
+    const deltaX = startX - e.touches[0].clientX;
+    draggableImage.style.left = `${deltaX}px`;
+    if (Math.abs(deltaX) > 50) { // Ajuste o valor conforme necessário
       reverseBtn.classList.remove('hidden');
     }
   }
@@ -52,6 +53,8 @@ draggableImage.addEventListener('touchmove', (e) => {
 draggableImage.addEventListener('touchend', () => {
   isDragging = false;
   draggableImage.style.cursor = 'grab';
+  draggableImage.style.left = '70%';
+  draggableImage.style.transform = 'translateX(-50%)';
 });
 
 // Exibir mensagem após 2 minutos se o áudio não tiver sido trocado
@@ -59,4 +62,4 @@ setTimeout(() => {
   if (!audioChanged) {
     alert("Dica: Tente retirar o tubo de memória do mcgucket");
   }
-}, 60000); // 120000 milissegundos = 2 minutos
+}, 120000); // 120000 milissegundos = 2 minutos
