0d6ec80066b57bcdb65ab820bcdeeb8184a803fb
Gabriel Lopes <55008126+gabriellcortes@users.noreply.github.com>
2024-09-06 14:09:56 -0300

    Update script_audio.js

--- script_audio.js
+++ script_audio.js
@@ -1,6 +1,7 @@
 const playBtn = document.getElementById('play-btn');
 const pauseBtn = document.getElementById('pause-btn');
 const reverseBtn = document.getElementById('reverse-btn');
+const resetBtn = document.getElementById('reset-btn');
 const audioPlayer = document.getElementById('audio-player');
 const audioSlider = document.getElementById('audio-slider');
 const draggableImage = document.getElementById('draggable-image');
@@ -30,14 +31,26 @@ reverseBtn.addEventListener('click', () => {
   reverseBtn.classList.add('hidden');
 });
 
+resetBtn.addEventListener('click', () => {
+  audioPlayer.currentTime = 0;
+  audioPlayer.pause();
+  playBtn.classList.remove('hidden');
+  pauseBtn.classList.add('hidden');
+});
+
 audioPlayer.addEventListener('timeupdate', () => {
   const value = (audioPlayer.currentTime / audioPlayer.duration) * 100;
   audioSlider.value = value;
+
+  if (audioPlayer.currentTime === audioPlayer.duration) {
+    playBtn.classList.remove('hidden');
+    pauseBtn.classList.add('hidden');
+  }
 });
 
 audioSlider.addEventListener('input', () => {
-const value = (audioSlider.value / 100) * audioPlayer.duration;
-audioPlayer.currentTime = value;
+  const value = (audioSlider.value / 100) * audioPlayer.duration;
+  audioPlayer.currentTime = value;
 });
 
 // Executa a ação após 1 minuto iniciado a página
